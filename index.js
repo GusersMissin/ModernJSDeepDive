995171a051ebfe920b16b4384c83e65598a392e1
Gusers Missin <missingusers@gmail.com>
2024-01-10 07:13:01 +0900

    Equal Anti-patterns add ===

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ console.log(false == '0');
 console.log(false == '0');
 console.log(false == null);
 console.log(false == undefined);
+
+console.log("=========================")
+//Use '==='
+console.log('0' === '');
+console.log(0 === '');
+console.log(0 === '0');
+console.log(false === 'false');
+console.log(false === '0');
+console.log(false === '0');
+console.log(false === null);
+console.log(false === undefined);
