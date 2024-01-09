6c52bf9a7a7d29ffc6a05fc8d22e7b07389f7ae9
Gusers Missin <missingusers@gmail.com>
2024-01-10 07:06:30 +0900

    Equal Anti-patterns

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-function foo() {
-    return
-        {}
-    // ASI -> return; {};
-    // Developer expect -> return {};
-}
-
-console.log(foo());
-
-var bar = function() {}
-(function() {})();
-// ASI -> var bar = function() {} (function() {})();
-// Developer expect ->> var bar = function () {}; (function() {})();
-// TypeError:(intermediate value)(...) is not a function
+//Equal AntiPatterns
+console.log('0' == '');
+console.log(0 == '');
+console.log(0 == '0');
+console.log(false == 'false');
+console.log(false == '0');
+console.log(false == '0');
+console.log(false == null);
+console.log(false == undefined);
