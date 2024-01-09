1a6799c1585b05b98ad33df229a898413a9b8817
Gusers Missin <missingusers@gmail.com>
2024-01-10 07:22:22 +0900

    objet is

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-//Equal AntiPatterns
-console.log('0' == '');
-console.log(0 == '');
-console.log(0 == '0');
-console.log(false == 'false');
-console.log(false == '0');
-console.log(false == '0');
-console.log(false == null);
-console.log(false == undefined);
+//implicit coercion
+console.log(5==5);
+console.log(5=='5');
 
-console.log("=========================")
-//Use '==='
-console.log('0' === '');
-console.log(0 === '');
-console.log(0 === '0');
-console.log(false === 'false');
-console.log(false === '0');
-console.log(false === '0');
-console.log(false === null);
-console.log(false === undefined);
+console.log(5===5);
+//Don't change data type
+console.log(5==='5');
+
+console.log("--------------------");
+
+//NaN
+console.log(NaN == NaN);
+console.log(NaN === NaN);
+
+console.log("--------------------");
+
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN(10));
+console.log(Number.isNaN(undefined));
+console.log(Number.isNaN(undefined + 1));
+
+console.log("--------------------");
+
+console.log(0 === -0);
+console.log(0 == -0);
+
+console.log("--------------------");
+
+console.log(Object.is(-0, +0));
+console.log(Object.is(NaN, NaN));
